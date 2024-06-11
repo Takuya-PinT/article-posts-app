af2d95d296e6d667e239270bf4888ead476383f9
Takuya Iwamoto <iwamoto.takuya@pintinc.jp>
2024-06-11 17:55:30 +0900

    バリデーション追加

--- src/routes/articles/new/+page.svelte
+++ src/routes/articles/new/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
+
 	export let form = $$props.form
-	console.log(form)
+	console.log(form?.errors.fieldErrors.title)
 </script>
 
 <svelte:head>
     <title>新規記事投稿</title>
 </svelte:head>
 
-<form method="POST">
+<form method="POST" use:enhance>
 	<div class="grid gap-6 mt-6">
 		<div>
 			<label for="title" class="block mb-2 text-sm font-medium text-gray-900">タイトル</label>
@@ -16,7 +18,12 @@
 				name="title"
 				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 				required
+				aria-invalid="{form?.errors.fieldErrors?.title ? 'true': undefined}"
+				aria-describedby="{form?.errors.fieldErrors?.title ? 'title-error' : undefined}"
 			/>
+			{#if form?.errors.fieldErrors?.title}
+				<p id="title-error" class="text-red-600 text-sm mt-1">{form.errors.fieldErrors.title}</p>
+			{/if}
 
 		</div>
 		<div>
@@ -27,7 +34,14 @@
 				rows="10"
 				class="border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 				required
+				aria-invalid="{form?.fields.content ? 'true' : undefined}"
+				aria-describedby="{form?.errors.fieldErrors.content ? 'content-error' : undefined}"
 			></textarea>
+			{#if form?.errors.fieldErrors?.content}
+				<p id="content-error" class="text-red-600 text-sm mt-1">
+					{form.errors.fieldErrors.content}
+				</p>
+			{/if}
 		</div>
 		<div>
 			<button
@@ -39,7 +53,7 @@
 		</div>
 	</div>
 </form>
- 
+
 <style>
 	/* field-sizing は TailwindCSS でサポートしていないスタイルなので、ここで定義している　*/
 	textarea {
